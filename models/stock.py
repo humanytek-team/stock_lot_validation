3ffe7d9a55e70100755e3cd70b146765c9591493
Ruben Dario Bravo <rbravo@hoatzin.org>
2018-03-15 23:23:00 -0400

    se agrega validacion para movimientos de stock

--- models/stock.py
+++ models/stock.py
@@ -47,26 +47,27 @@ class StockMoveLots(models.Model):
                 res['warning'] = {'title': _('Warning'), 'message': message}
         return res
 
-    #@api.multi
-    #def do_plus(self):
-        #res = {}
-        #if self.env.context.get('raw_material'):
-            #StockQuant = self.env['stock.quant']
-            #quants = StockQuant.search([
-                    #('product_id.id', '=', self.product_id.id),
-                    #('lot_id.id', '=', self.lot_id.id),
-                    #('location_id.id', '=', self.env.context.get('location_id'))
-                    #])
-            #qty_total = 0
-            #for quant in quants:
-                #qty_total += quant.qty
-            #if self.quantity_done > qty_total:
-                #self.quantity_done = 0
-                #message = "No tiene stock de este lote"
-                #res['warning'] = {'title': _('Warning'), 'message': message}
-        #if res:
-            #return res
-        #else:
-            #return super(StockMoveLots, self).do_plus()
 
+class StockPackOperationLot(models.Model):
+    _inherit = 'stock.pack.operation.lot'
+
+    @api.onchange('lot_id', 'qty')
+    def onchange_lot(self):
+        res = {}
+        if self.env.context.get('field_parent') and self.env.context.get('field_parent') == 'operation_id':
+            StockQuant = self.env['stock.quant']
+            quants = StockQuant.search([
+                    ('product_id.id', '=',
+                        self.env.context.get('default_product_id')),
+                    ('lot_id.id', '=', self.lot_id.id),
+                    ('location_id.id', '=', self.env.context.get('location_id'))
+                    ])
+            qty_total = 0
+            for quant in quants:
+                qty_total += quant.qty
+            if self.qty > qty_total:
+                self.qty = 0
+                message = "No tiene stock de este lote"
+                res['warning'] = {'title': _('Warning'), 'message': message}
+        return res
 
